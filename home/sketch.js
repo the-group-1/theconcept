24112510c76d76344400bfe720fce0f6e3f75a5d
anasraza-zz <anasraza.go@gmail.com>
2019-11-19 21:03:57 -0500

    rotate

--- home/sketch.js
+++ home/sketch.js
@@ -1,7 +1,35 @@
+let titlepage;
+let currentPosition, previousPosition;
+
+function preload(){
+  titlepage = loadImage("img/booktitle.jpg");
+}
+
+
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
+  image(titlepage);
 }
 
 function draw() {
   background(220);
+  fill(0);
+  line(width/2,0,width/2,height);
+  if (mouseX>=width/2) {
+   currentPosition = "RIGHT"
+ }else {
+   currentPosition = "LEFT"
+ }
+
+ if (currentPosition != previousPosition) {
+ console.log(currentPosition);
+ }
+
+
+fill(0);
+textSize(30);
+text(rotationZ,0,height/2);
+
+
 }
